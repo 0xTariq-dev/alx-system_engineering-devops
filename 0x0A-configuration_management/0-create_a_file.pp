2b8057f369eb4bea023ae194a1a20d5dc1bf1e69
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-23 01:19:31 +0200

    feat: Create a file in '/tmp' Using puppet.

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Create a file called 'School' in '/tmp' directory and write some text into it.
+file { '/tmp/School':
+  ensure  => 'present',
+  content => 'I Love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  }
