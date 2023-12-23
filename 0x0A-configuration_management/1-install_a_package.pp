df97d179e74995eb2a43ba76fe97e28047318a8f
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-23 02:11:24 +0200

    fix: 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,14 @@
 #!/usr/bin/pup
 # Using Puppet, install flask(2.1.0) from pip3.
-package { 'python3':
+package { 'python':
   ensure   => '3.8.10',
-  provider => 'pip3',
+  provider => 'apt',
 }
 package { 'Werkzeug ':
   ensure   => '2.1.1',
   provider => 'pip3',
 }
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  require => [Package['python3'], Package['Werkzeug']],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
