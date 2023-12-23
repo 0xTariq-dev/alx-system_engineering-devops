f088e46230c97d4faa9018c80a2fef1791c40335
0xTariq-dev <tariqmohsen222@gmail.com>
2023-12-23 09:56:11 +0200

    fix: 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Create a file called 'School' in '/tmp' directory and write some text into it.
 
 file { '/tmp/school':
-  content =>'I Love Puppet',
+  content =>'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
