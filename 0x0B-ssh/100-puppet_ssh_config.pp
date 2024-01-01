d6d634a70512132fdaf54d59e41388e862db96ec
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-01-01 20:15:23 +0200

    fix: Puppet script to edit ssh_config file.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 include stdlib
 
-file_line { 'Turn off password authentication':
+file_line { 'Turn off passwd auth':
   path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
   match => '^\s*PasswordAuthentication no',
 }
 
-file_line { 'Set IdentityFile':
+file_line { 'Declare identity file':
   path  => '/etc/ssh/ssh_config',
   line  => 'IdentityFile ~/.ssh/school',
   match => '^\s*IdentityFile',
