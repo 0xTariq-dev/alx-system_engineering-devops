13adbb304d6d1a31ec5702acb4db8d70430cd067
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-03-06 17:07:10 +0200

    feat: function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -17,4 +17,3 @@ def top_ten(subreddit):
     posts = response.json().get('data', {}).get('children', [])
     for post in posts:
         print(post.get('data', {}).get('title', ''))
-    # print(post.get('data', {}).get('title', '') for post in posts)
