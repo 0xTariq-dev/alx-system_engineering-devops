0ec7c16510cee91e0e3b17edd4c87e2a81ffca34
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-03-08 04:13:14 +0200

    fix: Puppet mainfest to fix a wordpress site running on a LAMP stack. #1

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,8 @@
+#!/usr/bin/puppet
 # This is a sample Puppet manifest for fixing a typo in a file a php file.
 
 exec { 'fix wordpress typo':
-  command  => "sudo sed -i 's/.phpp /.php/g' /var/www/html/wp-settings.php",
+  command  => "sudo sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
   provider => shell,
 }
 
